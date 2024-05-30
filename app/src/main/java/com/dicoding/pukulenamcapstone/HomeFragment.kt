d43cda85ff756a692290b674d3cc1dfb6ab431db
abi-widiarta <abiwidiarta03@gmail.com>
2024-05-30 16:30:53 +0700

    feat: add all news fragment and finish news detail fragment

--- app/src/main/java/com/dicoding/pukulenamcapstone/HomeFragment.kt
+++ app/src/main/java/com/dicoding/pukulenamcapstone/HomeFragment.kt
@@ -7,6 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.Navigation
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -29,16 +31,20 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
-
-
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val button = view.findViewById<TextView>(R.id.textView16)
+        button.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_homeFragment_to_newsDetailFragment)
+        }
 
+        val button2 = view.findViewById<Button>(R.id.button2)
+        button2.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_homeFragment_to_allNewsFragment)
+        }
     }
-
-
 }
